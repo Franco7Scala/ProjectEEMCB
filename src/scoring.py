127959b7c87c8fcd0b421622bb71b5890de96a82
franco7scala <franco7scala@gmail.com>
2018-11-07 17:33:51 +0100

    several improvements to svm code

---
+++ src/scoring.py
@@ -0,0 +1,50 @@
+from __future__ import division
+from sklearn.utils.validation import check_array, check_consistent_length
+from sklearn.externals.six import string_types
+
+import numpy
+
+
+def relative_error(y_true, y_pred, sample_weight=None, multioutput='uniform_average'):
+    y_type, y_true, y_pred, multioutput = _check_reg_targets(y_true, y_pred, multioutput)
+    check_consistent_length(y_true, y_pred, sample_weight)
+    output_errors = numpy.average(abs((y_true - y_pred) / y_true) * 100, axis=0, weights=sample_weight)
+    if isinstance(multioutput, string_types):
+        if multioutput == 'raw_values':
+            return output_errors
+        elif multioutput == 'uniform_average':
+            multioutput = None
+    return numpy.average(output_errors, weights=multioutput)
+
+
+def _check_reg_targets(y_true, y_pred, multioutput):
+    check_consistent_length(y_true, y_pred)
+    y_true = check_array(y_true, ensure_2d=False)
+    y_pred = check_array(y_pred, ensure_2d=False)
+    if y_true.ndim == 1:
+        y_true = y_true.reshape((-1, 1))
+    if y_pred.ndim == 1:
+        y_pred = y_pred.reshape((-1, 1))
+    if y_true.shape[1] != y_pred.shape[1]:
+        raise ValueError("y_true and y_pred have different number of output "
+                         "({0}!={1})".format(y_true.shape[1], y_pred.shape[1]))
+    n_outputs = y_true.shape[1]
+    allowed_multioutput_str = ('raw_values', 'uniform_average',
+                               'variance_weighted')
+    if isinstance(multioutput, string_types):
+        if multioutput not in allowed_multioutput_str:
+            raise ValueError("Allowed 'multioutput' string values are {}. "
+                             "You provided multioutput={!r}".format(
+                                 allowed_multioutput_str,
+                                 multioutput))
+    elif multioutput is not None:
+        multioutput = check_array(multioutput, ensure_2d=False)
+        if n_outputs == 1:
+            raise ValueError("Custom weights are useful only in "
+                             "multi-output cases.")
+        elif n_outputs != len(multioutput):
+            raise ValueError(("There must be equally many custom weights "
+                              "(%d) as outputs (%d).") %
+                             (len(multioutput), n_outputs))
+    y_type = 'continuous' if n_outputs == 1 else 'continuous-multioutput'
+    return y_type, y_true, y_pred, multioutput
