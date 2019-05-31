ace9379928e29ab2f71f1106d66fbb14dac23f14
franco7scala <franco7scala@gmail.com>
2019-05-31 16:29:12 +0200

    addded some lines in installation script

--- src/production/configuration/install.sh
+++ src/production/configuration/install.sh
@@ -1,6 +1,25 @@
 #!/usr/bin/env bash
 
+# installing pip
+sudo apt-get install python-pip
 
+# installing required libraries
+sudo pip install numpy
+sudo pip install sklearn
+sudo pip install MySQL-python
+
+
+
+
+
+
+
+
+
+
+
+
+
+##############
 sudo apt-get install python-pip python-dev libmysqlclient-dev
-pip install MySQL-python
 sudo apt-get install python-mysqldb
